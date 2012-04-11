9b09374c27b31101c4eff508d81dc42f605939f3
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2012-04-11 21:35:12 +0100

    Stuff for kanban

--- server.js
+++ server.js
@@ -2,9 +2,3 @@ var server
 , http     = require('./http.js');
         
 http.createServer().server;
-
-// var http = require('http');
-// http.createServer(function (req, res) {
-//   res.writeHead(200, {'Content-Type': 'text/plain'});
-//   res.end('Hello World\nApp (kaizenkanban/) is running..');
-// }).listen(8861);
