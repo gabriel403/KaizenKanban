46e50c7bcaecce35e874cf739a6886478dea6e19
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2012-04-20 14:13:27 +0100

    Reworked more js into the js file changed some css for table ty @SpacedMonkeyTCT columns are created by js now instead

--- kk/dndCreator.js
+++ kk/dndCreator.js
@@ -1,25 +1,49 @@
 define(
-    [ "dojo/string", "dojo/dom-construct", "dojo/dom-class", "dojo/dnd/Source", "dojo/text!/kk/kanbanCard.html" ],
-    function(stringUtil, domConstruct, domClass, Source, template){
+    [ "dojo/string", "dojo/dom-construct", "dojo/dom-class", "dojo/dnd/Source", "dojo/text!/kk/kanbanCard.html", "dojo/text!/kk/kanbanColumn.html" ],
+    function(stringUtil, domConstruct, domClass, Source, cardTemplate, columnTemplate){
+
+        function removeSource(show, source, nodes){
+            dojo.style(source[0], 'visibility', 'hidden');
+        }
+        function displaySource(show, source, nodes){
+            dojo.style(source[0], 'visibility', 'visible');
+        }
+
+        function addListeners(){
+            dojo.subscribe("/dnd/start", removeSource);
+            dojo.subscribe("/dnd/drop", displaySource);
+            // dojo.subscribe("/dnd/cancel, /dnd/drop", displaySource);
+
+        }
 
         // create the DOM representation for the given item
         function cardCreator(item){
             var node = domConstruct.toDom(
                 stringUtil.substitute(
-                    template,
+                    cardTemplate,
                     {
-                        name: item.name,
-                        quantity: item.quantity,
-                        description: item.description
+                        name:           item.name,
+                        quantity:       item.quantity,
+                        description:    item.description
                     }
                 )
-            ),
-            type = item.quantity ? [ "inStock" ] : [ "outOfStock" ];
-            return { node: node, data: item, type: type };
+            );
+            return { node: node, data: item };
         }
 
+        // create the DOM representation for the given item
         function workflowStepCreator(item){
-            
+            var node = domConstruct.toDom(
+                stringUtil.substitute(
+                    columnTemplate,
+                    {
+                        title: item.title,
+                        id: item.id
+                    }
+                )
+            );
+            domConstruct.place(node,"innerContainer");
+            return node;
         }
 
         // creates a dojo/dnd/Source from the data provided
@@ -28,31 +52,28 @@ define(
             // create the Source
             var dndObj = new Source(node, {
                 // ensure that only move operations ever occur from this source
-                copyOnly: false,
+                copyOnly:   false,
 
                 // define whether or not this source will accept drops from itself, based on the value passed into
                 // buildCatalog; defaults to true, since this is the default that dojo/dnd uses
                 selfAccept: selfAccept === undefined ? true : selfAccept,
 
                 // use catalogNodeCreator as our creator function for inserting new nodes
-                creator: cardCreator
+                creator:    cardCreator
             });
 
             // insert new nodes to the Source; the first argument indicates that they will not be highlighted (selected)
             // when inserted
             dndObj.insertNodes(false, data);
 
-            // add CSS hooks for element styling
-            dndObj.forInItems(function(item, id, map){
-                domClass.add(id, item.type[0]);
-            });
-
             return dndObj;
         }
 
         // expose our API
         return {
-            buildCardList: buildCardList
+            buildCardList:          buildCardList,
+            workflowStepCreator:    workflowStepCreator,
+            addListeners:           addListeners
         };
     }
 );
