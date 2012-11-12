8e8395455471993810490ce8448775f9662ad576
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2012-11-12 13:45:10 +0000

    tweaks

--- source/server.js
+++ source/server.js
@@ -2,5 +2,5 @@
 var sl     	= require('./server-js/serviceLocator.js');
 // var http     = require('./server-js/http.js');
 var http 	= sl.get('kkhttp');
-http.createServer(8861);
+http.createServer(process.env.VCAP_APP_PORT || 8861);
 console.log(http.connectionToString());
