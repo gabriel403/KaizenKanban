8e618f96ad4e9e2f1be1d9f48489aedf05bea4e4
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2012-11-12 14:11:25 +0000

    heroku hack

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
 //hack for cloudeno.de
-console.log("cloudenode hack");
-require('./source/server.js');
+"use strict";
+console.log("heroku hack");
+var sl     	= require('./source/server-js/serviceLocator.js');
+// var http     = require('./server-js/http.js');
+var http 	= sl.get('kkhttp');
+http.createServer(process.env.PORT, "0.0.0.0");
+console.log(http.connectionToString());
