bd5c201a5a6da8834364a0079d7eab0130158ab5
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2012-11-17 20:58:18 +0000

    heroku hack

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 "use strict";
-var sl     	= require('./server-js/serviceLocator.js');
+var sl     	= require('./source/server-js/serviceLocator.js');
 // var http     = require('./server-js/http.js');
 var http 	= new (sl.get('kkhttp').http);
 http.createServer(process.env.PORT, "0.0.0.0");
