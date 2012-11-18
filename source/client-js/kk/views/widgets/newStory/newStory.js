c2c2cdfe31bcad4c30edd849724cdb6ee44e1a9f
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2012-11-18 00:15:04 +0000

    tweaks to use attach data dojo attach points, css tweaks

--- source/client-js/kk/views/widgets/newStory/newStory.js
+++ source/client-js/kk/views/widgets/newStory/newStory.js
@@ -1,30 +1,24 @@
 define([ "dojo/_base/declare", "dijit/form/ValidationTextBox", "dijit/form/ComboBox", "dijit/Dialog", "dijit/form/Button",
 	"dijit/_WidgetBase", "dijit/_TemplatedMixin", "dijit/_WidgetsInTemplateMixin", "dojo/text!./newStory.html", 
-    "dijit/registry" ],
-    function(declare, ValidationTextBox, ComboBox, Dialog, Button,
-    	_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, template,
-        registry){
-        return declare([_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
-            baseClass       : "newStoryCardWidget",
-            templateString  : template,
-        	parentView		: null,
-        	name			: null,
-        	description		: null,
-        	workflow		: null,
-            store           : null,
-            postCreate      : function(){
-                this.name           = registry.byId('storynameIpt');
-                this.description    = registry.byId('storydescIpt');
-                this.workflow       = registry.byId('workflowCmbBx');
-                this.workflow.store = this.store
-                // this.store  = this.parentView.mainModel.workflowStore;
-            },
-        	setupDijits		: function() {
-        		// this.name			= new ValidationTextBox();
-        		// this.description	= new ValidationTextBox();
-        		// this.workflow		= new ComboBox({
-        		// 	store: this.parentView.mainModel.workflowStore
-        		// });
-        	}
-        });
+	"dijit/registry", "dijit/form/Form", "dojo/query", "dijit/registry" ],
+	function(declare, ValidationTextBox, ComboBox, Dialog, Button,
+		_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, template,
+		registry, Form, query, registry){
+		return declare([_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
+			baseClass			: "newStoryCardWidget",
+			templateString		: template,
+			parentView			: null,
+			storynameIpt		: null,
+			storydescIpt		: null,
+			workflowCmbBx		: null,
+			store				: null,
+			newStoryForm		: null,
+			postCreate			: function(){
+				this.workflowCmbBx.store	= this.store
+				// this.store  = this.parentView.mainModel.workflowStore;
+			},
+			newStoryValidation	: function() {
+				console.log(newStoryForm.validate());
+			}
+		});
 });
