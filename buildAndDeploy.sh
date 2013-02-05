6d8c719a827effe0111ddd97de2a2ec4a2541674
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2013-02-05 21:51:15 +0000

    new build and sdeploy script

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -1,26 +1,55 @@
 #!/bin/sh
 
+
+SRCS=( dojo dojox dijit util library kk )
+
+SAVE_IFS=$IFS
+IFS=" "
+SRCSTR="${SRCS[*]}"
+IFS=$SAVE_IFS
+
+SRCBAKDIR=dtk
+DEPLOYDIR=deploy
+DEPLOYDEST=heroku
+DEPLOYBRANCH=master
+
 set -x
 
 # prep for build
 cd src/
 rm css/dojo/nihilo.css css/dojo/dojo.css css/dojo/dnd.css
 
-cd client-js/
-rm -rf nls dojo.js
 
 # build
-cd dtk/util/buildscripts/
-./build.sh --dojoConfig ../../../dojoConfig.js --profile ../../../build.profile.js --release
+cd client-js/util/buildscripts/
+./build.sh --dojoConfig ../../dojoConfig.js --profile ../../build.profile.js --release
 
-# copy built files to dest
-cd ../../../
-cp -R deploy/dojo/dojo.js deploy/dojo/nls .
-cp deploy/dijit/themes/nihilo/nihilo.css deploy/dojo/resources/dojo.css deploy/dojo/resources/dnd.css ../css/dojo/
+# backup no deployable files to gitignored directory
+cd ../../
+mv $SRCSTR $SRCBAKDIR
 
-# cleanup
-# rm -rf deploy
+# cleanup - stage 1
 find . -type f -name '*.uncompressed.js' -print0 | xargs -0 rm -rdf
 find . -type f -name '*.consoleStripped.js' -print0 | xargs -0 rm -rdf
+
+# copy built files to dest
+mv ${DEPLOYDIR}/dijit/themes/nihilo/nihilo.css ${DEPLOYDIR}/dojo/resources/dojo.css ${DEPLOYDIR}/dojo/resources/dnd.css ../css/dojo/
+mv ${DEPLOYDIR}/* .
+
+# cleanup - stage 2
+rm -rf ${DEPLOYDIR}
+
+# deploy
+#git commit -am "deploying"
+#git push -u ${DEPLOYDEST} ${DEPLOYBRANCH}
+
+# destroy built files
+#rm -rf $SRCSTR
+
+# restore backup
+#mv ${SRCBCK}/* .
+
+# remove defunct commit
+#git reset --hard HEAD~1
 
 set +x
